8f806b4b3cc55489ed14eb9b97cce64a05bdf8e8
Romain Sanchez <romainsancho@gmail.com>
2016-09-15 12:18:08 +0200

    Added Dropzone form type for file uploads

---
+++ Form/Type/DropzoneType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Librinfo\EmailBundle\Form\Type;
+
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Librinfo\CoreBundle\Form\AbstractType as BaseAbstractType;
+
+/**
+ * File upload form type
+ */
+class DropzoneType extends BaseAbstractType
+{
+    public function getParent()
+    {
+        return 'form';
+    }
+    
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'librinfo_dropzone';
+    }
+
+}
