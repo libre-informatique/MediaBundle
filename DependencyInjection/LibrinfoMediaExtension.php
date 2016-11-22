89ac71ae992dd698838ab841b6eb35fb79f59da2
Romain <romain.sanchez@libre-informatique.fr>
2016-11-22 16:37:42 +0100

    removed unnecessary override of LibrinfoCoreExtension::load()

--- DependencyInjection/LibrinfoMediaExtension.php
+++ DependencyInjection/LibrinfoMediaExtension.php
@@ -2,9 +2,6 @@
 
 namespace Librinfo\MediaBundle\DependencyInjection;
 
-use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\Config\FileLocator;
-use Symfony\Component\DependencyInjection\Loader;
 use Librinfo\CoreBundle\DependencyInjection\LibrinfoCoreExtension;
 
 /**
@@ -14,18 +11,4 @@ use Librinfo\CoreBundle\DependencyInjection\LibrinfoCoreExtension;
  */
 class LibrinfoMediaExtension extends LibrinfoCoreExtension
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function load(array $configs, ContainerBuilder $container)
-    {
-        $configuration = new Configuration();
-        $this->processConfiguration($configuration, $configs);
-
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
-        $loader->load('services.yml');
-        $loader->load('admin.yml');
-
-        $this->mergeParameter('librinfo', $container, __DIR__ . '/../Resources/config');
-    }
 }
